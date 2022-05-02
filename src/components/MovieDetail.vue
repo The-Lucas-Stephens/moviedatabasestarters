b4043bd364176b9186906905880d188cf86199ff
STEPHENS <ls0930792@otc.edu>
2022-05-02 12:57:10 -0500

    movie db

---
+++ src/components/MovieDetail.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>{{movieobj.title}}</h1>
+
+        <p>{{id}}</p>
+    </div>
+
+    
+</template>
+
+<script>
+import axios from "axios"
+export default{
+    name:"MovieDetail",
+    data(){
+        return{
+            id:this.$route.params.id,
+            movieobj:Object
+        }
+    },
+    mount(){
+        const request=`https://api.themoviedb.org/3/movie/${this.id}?api_key=5134b3f56c2cae575bb0ad435f0be5ee&language=en-US`
+        console.log(request)
+
+        axios.get(request)
+        .then( (request)=>{
+            this.movieobj = request.data
+        })
+        
+    }
+    
+
+}
+</script>
--- src/components/MoviesDb.vue
+++ src/components/MoviesDb.vue
@@ -1,13 +1,39 @@
 <template>
     <div>
         <h1>Upcoming Movies</h1>
-      
+        <div v-bind:key="movie.id" v-for="movie in movies" >
+            <h3>{{movie.title}}</h3>
+            <router-link :to="`/movie/${movie.id}`"> Details for: {{movie.title}}</router-link>
+        </div>
     </div>
 </template>
 
 <script>
-
+import axios from "axios"
 export default{
-    name:"MoviesDb"
+    name:"MoviesDb",
+    data(){
+        return{
+            movies:[]
+        }
+    },
+    mounted(){
+        axios
+        .get(
+          "https://api.themoviedb.org/3/movie/upcoming?api_key=5134b3f56c2cae575bb0ad435f0be5ee&language=en-US&page=1"
+        )
+        .then((response) => {
+          console.log(response);
+          this.movies= response.data.results
+          console.log(this.movies);
+  
+          return this.someMovies;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
 }
+
+
 </script>
